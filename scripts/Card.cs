a4516772d50bed81a699c1ee782bf573845b8679
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-10 20:41:11 -0500

    Used the internet to figure out how to click on things
    At least that part works

--- scripts/Card.cs
+++ scripts/Card.cs
@@ -4,23 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 
 public partial class Card : Area2D
-{
+{	
+	private Main main;
 	private Card card;
 	private Button button;
-	private Area2D mouse_area;
 	public Label card_label;
 	public List<Area2D> cards = new List<Area2D>();
 
 	public bool is_revealed = false;
-	private bool is_mouse_hovering = false;
 
 	public override void _Ready()
 	{
 		card_label = GetNode<Label>("card_label");
 		button = GetNode<Button>("card_button");
-		mouse_area = GetNode<Area2D>("mouse_area");
+		main = GetNode<Main>("../../../../main");
 		card_label.Visible = false;
-		cards.Add(this);
+	
 	}
 
 	//All the logic and counters here simply do not work
@@ -29,18 +28,7 @@ public partial class Card : Area2D
 	//A new way of figuring it out
 	public override void _Process(double delta)
 	{
-		mouse_area.Position = GetLocalMousePosition();
-		if(is_mouse_hovering)
-		{
-			if(Input.IsMouseButtonPressed(MouseButton.Left))
-			{
-				if(card_label.Text == card.card_label.Text)
-				{
-					is_revealed = true;
-					card.is_revealed = true;
-				}
-			}
-		}
+		
 		if(is_revealed)
 		{
 			card_label.Visible = true;
@@ -64,33 +52,23 @@ public partial class Card : Area2D
 		}
 	}
 
-	private void _on_mouse_area_area_entered(Area2D area)
+	private void _on_input_event(Node viewport, InputEvent @event, int shape_idx)
 	{
-		if(area is Card)
+		if(@event.IsActionPressed("click"))
 		{
-			card = (Card)area;
+			if(is_revealed)
+			{
+				is_revealed = false;
+			}
+			else if(is_revealed == false)
+			{
+				is_revealed = true;
+			}
+			main.card_clicked_counter++;
+			
 		}
 	}
 
-	private void _on_mouse_area_area_exited(Area2D area)
-	{
-		if(area is Card)
-		{
-			card = null;
-		}
-	}
-
-	private void _on_mouse_entered()
-	{
-		is_mouse_hovering = true;
-	}
-
-	private void _on_mouse_exited()
-	{
-		is_mouse_hovering = false;
-	}
-
-	
 	public void hide_all_cards()
 	{
 		is_revealed = false;
--- scripts/Main.cs
+++ scripts/Main.cs
@@ -5,98 +5,18 @@ using System.Linq;
 
 public partial class Main : Node2D
 {
-	private Area2D mouse_area;
-	private Card first_card;
-	private Card second_card;
-	public List<Area2D> cards = new List<Area2D>();
+	
 
-	private bool is_mouse_hovering = false;
-	private bool is_first_card_chosen = false;
-	private bool is_first_card_clicked = false;
-	private bool is_second_card_chosen = false;
-	private bool is_second_card_clicked = false;
-
-	private bool is_first_counter_added = false;
-	private bool is_second_counter_added = false;
-
-	private int card_clicked_counter = 0;
+	public int card_clicked_counter = 0;
 	public override void _Ready()
 	{	
-		mouse_area = GetNode<Area2D>("mouse_area");
+		
 	}
 
 	
 	public override void _Process(double delta)
 	{
-		//mouse_area.Position = GetLocalMousePosition();
-
-		//Let's try a new tactic, reveal all clicked cards
-		//Sweep the board after two cards are revealed
-		//All cards, unless the ones clicked before if it's a pair
-
-		/*if(Input.IsMouseButtonPressed(MouseButton.Left))
-		{
-			if(is_mouse_hovering)
-			{
-				if(is_first_card_chosen)
-				{
-					first_card.card_label.Visible = true;
-					is_first_card_clicked = true;
-					
-				}
-				else if(is_second_card_chosen)
-				{
-					second_card.card_label.Visible = true;
-					is_second_card_clicked = true;
-					
-				}
-			}
-		}
-
-		if(is_first_card_clicked && is_second_card_clicked)
-		{
-			first_card.is_revealed = false;
-			second_card.is_revealed = false;
-			if(first_card.card_label.Text == second_card.card_label.Text)
-			{
-				first_card.is_revealed = true;
-				second_card.is_revealed = true;
-			}
-			is_first_card_chosen = false;
-			is_second_card_chosen = false;
-			is_first_card_clicked = false;
-			is_second_card_clicked = false;
-			card_clicked_counter = 0;
-		}
-		*/
+		
 	}
 
-	private void _on_mouse_area_area_entered(Area2D area)
-	{
-		if(area is Card)
-		{
-			if(is_first_card_chosen == false)
-			{
-				first_card = (Card)area;
-				is_first_card_chosen = true;
-			}
-			else if(is_first_card_chosen)
-			{
-				second_card = (Card)area;
-				is_second_card_chosen = true;
-			}
-			
-			is_mouse_hovering = true;
-		}
-	}
-
-	private void _on_mouse_area_area_exited(Area2D area)
-	{
-		if(area is Card)
-		{
-			is_mouse_hovering = false;
-		}
-	}
-
-	
 }
